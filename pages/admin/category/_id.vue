379752ce3d94f1bfa462de863b32edaff8f60d94
serii <seriiburduja@gmail.com>
2022-09-03 21:31:55 +0300

    feat: edit category

--- pages/admin/category/_id.vue
+++ pages/admin/category/_id.vue
@@ -1,44 +1,40 @@
-<template>
-  <AdminForm label="Create category">
-    <div class="form__flex">
-      <div class="form__item" :class="{ 'form__item--error': errors.name }">
-        <label class="form__label" htmlFor="name"> Name </label>
-        <input type="text" placeholder="Enter name..." v-model="name" />
-        <p v-if="errors && errors.name" class="text-error">
-          {{ errors.name[0] }}
-        </p>
-      </div>
-      <div class="form__item">
-        <label class="form__label" htmlFor="status"> Status </label>
-        <select name="status" id="status" v-model="status">
-          <option :value="1">Active</option>
-          <option :value="0">Inactive</option>
-        </select>
-      </div>
-    </div>
-    <button class="btn" @click="onSubmit">Submit</button>
-  </AdminForm>
+<template lang="pug">
+AdminForm(label="Edit category")
+  .form__flex
+    .form__item(:class="{ 'form__item--error': errors.title }")
+      label.form__label(for="title") Title
+      input(type="text", placeholder="Enter title...", v-model="title")
+      p.text-error(v-if="errors && errors.name")
+        | {{ errors.title[0] }}
+    .form__item
+      label.form__label(for="parent_id") Categories
+      select#parent_id(name="parent_id" v-model="parent_id")
+        option(value="0") No parent
+        option(v-for="item in categories" :key="item.id" :value="item.id") {{ item.title }}
+  button.btn(@click="onSubmit") Submit
 </template>
 <script>
 import AdminForm from "@/admin/form/Form";
 export default {
+  layout: "admin",
   data() {
     return {
       id: "",
-      name: "",
-      status: 1,
+      title: "",
+      parent_id: 0,
+      categories: [],
       errors: {},
     };
   },
   methods: {
     onSubmit(e) {
       e.preventDefault();
-      const data = { name: this.name, status: this.status };
+      const data = { title: this.title, status: this.status };
 
       this.$axios
-        .put("/category/" + this.id, data)
+        .put("/auth/category/" + this.id, data)
         .then(() => {
-          this.$router.push("/category");
+          this.$router.push("/admin/category");
         })
         .catch((err) => {
           if (err.response.data && err.response.data.errors) {
@@ -48,11 +44,29 @@ export default {
     },
     getData() {
       this.$axios
-        .get("/category/" + this.id)
+        .get("/auth/category/" + this.id)
         .then((res) => {
-          const { name, status } = res.data.data;
-          this.name = name;
-          this.status = status;
+          console.log(res.data.data, "res.data.data");
+          const { title, parent_id } = res.data.data;
+          this.title = title;
+          this.parent_id = parent_id;
+          console.log(this.parent_id, "this.parent_id");
+        })
+        .catch((err) => {
+          if (err.response.data && err.response.data.errors) {
+            setErrors(err.response.data.errors);
+          }
+        });
+    },
+    getCategories() {
+      this.$axios
+        .get("/auth/category")
+        .then((res) => {
+          const categories = res.data.data;
+          this.categories = categories.filter((item) => {
+            return item.parent_id === 0;
+          });
+          console.log(this.categories, "this.categories");
         })
         .catch((err) => {
           if (err.response.data && err.response.data.errors) {
@@ -67,6 +81,7 @@ export default {
   created() {
     this.id = this.$route.params.id;
     this.getData();
+    this.getCategories();
   },
 };
 </script>
