58694ab1422332c2966c38050b14944857a62122
serii <seriiburduja@gmail.com>
2022-09-05 10:09:16 +0300

    feat: category crud

--- pages/admin/category/_id.vue
+++ pages/admin/category/_id.vue
@@ -1,20 +1,21 @@
 <template lang="pug">
-AdminForm(label="Edit category")
-  .form__flex
-    .form__item(:class="{ 'form__item--error': errors.title }")
-      label.form__label(for="title") Title
-      input(type="text", placeholder="Enter title...", v-model="title")
-      p.text-error(v-if="errors && errors.name")
-        | {{ errors.title[0] }}
-    .form__item
-      label.form__label(for="parent_id") Categories
-      select#parent_id(name="parent_id" v-model="parent_id")
-        option(value="0") No parent
-        option(v-for="item in categories" :key="item.id" :value="item.id") {{ item.title }}
-  button.btn(@click="onSubmit") Submit
+  AdminForm(label="Edit category")
+    .form__flex
+      .form__item(:class="{ 'form__item--error': errors.title }")
+        label.form__label(for="title") Title
+        input(type="text", placeholder="Enter title...", v-model="title")
+        p.text-error(v-if="errors && errors.title")
+          | {{ errors.title[0] }}
+      .form__item
+        label.form__label(for="parent_id") Categories
+        select#parent_id(name="parent_id" v-model="parent_id")
+          option(value="0") No parent
+          option(v-for="item in categories" :key="item.id" :value="item.id") {{ item.title }}
+    button.btn(@click="onSubmit") Submit
 </template>
 <script>
 import AdminForm from "@/admin/form/Form";
+
 export default {
   layout: "admin",
   data() {
@@ -29,50 +30,50 @@ export default {
   methods: {
     onSubmit(e) {
       e.preventDefault();
-      const data = { title: this.title, status: this.status };
+      const data = {title: this.title, status: this.status, parent_id: this.parent_id};
 
       this.$axios
-        .put("/auth/category/" + this.id, data)
-        .then(() => {
-          this.$router.push("/admin/category");
-        })
-        .catch((err) => {
-          if (err.response.data && err.response.data.errors) {
-            this.errors = err.response.data.errors;
-          }
-        });
+          .put("/auth/category/" + this.id, data)
+          .then(() => {
+            this.$router.push("/admin/category");
+          })
+          .catch((err) => {
+            if (err.response.data && err.response.data.errors) {
+              this.errors = err.response.data.errors;
+            }
+          });
     },
     getData() {
       this.$axios
-        .get("/auth/category/" + this.id)
-        .then((res) => {
-          console.log(res.data.data, "res.data.data");
-          const { title, parent_id } = res.data.data;
-          this.title = title;
-          this.parent_id = parent_id;
-          console.log(this.parent_id, "this.parent_id");
-        })
-        .catch((err) => {
-          if (err.response.data && err.response.data.errors) {
-            setErrors(err.response.data.errors);
-          }
-        });
+          .get("/auth/category/" + this.id)
+          .then((res) => {
+            console.log(res.data.data, "res.data.data");
+            const {title, parent_id} = res.data.data;
+            this.title = title;
+            this.parent_id = parent_id;
+            console.log(this.parent_id, "this.parent_id");
+          })
+          .catch((err) => {
+            if (err.response.data && err.response.data.errors) {
+              setErrors(err.response.data.errors);
+            }
+          });
     },
     getCategories() {
       this.$axios
-        .get("/auth/category")
-        .then((res) => {
-          const categories = res.data.data;
-          this.categories = categories.filter((item) => {
-            return item.parent_id === 0;
+          .get("/auth/category")
+          .then((res) => {
+            const categories = res.data.data;
+            this.categories = categories.filter((item) => {
+              return item.parent_id === 0;
+            });
+            console.log(this.categories, "this.categories");
+          })
+          .catch((err) => {
+            if (err.response.data && err.response.data.errors) {
+              setErrors(err.response.data.errors);
+            }
           });
-          console.log(this.categories, "this.categories");
-        })
-        .catch((err) => {
-          if (err.response.data && err.response.data.errors) {
-            setErrors(err.response.data.errors);
-          }
-        });
     },
   },
   components: {
--- pages/admin/category/create.vue
+++ pages/admin/category/create.vue
@@ -1,53 +1,69 @@
-<template>
-  <AdminForm label="Create category">
-    <div class="form__flex">
-      <div class="form__item" :class="{ 'form__item--error': errors.name }">
-        <label class="form__label" htmlFor="name"> Name </label>
-        <input type="text" placeholder="Enter name..." v-model="name" />
-        <p v-if="errors && errors.name" class="text-error">
-          {{ errors.name[0] }}
-        </p>
-      </div>
-      <div class="form__item">
-        <label class="form__label" htmlFor="status"> Status </label>
-        <select name="status" id="status" v-model="status">
-          <option :value="1">Active</option>
-          <option :value="0">Inactive</option>
-        </select>
-      </div>
-    </div>
-    <button class="btn" @click="onSubmit">Submit</button>
-  </AdminForm>
+<template lang="pug">
+  AdminForm(label="Edit category")
+    .form__flex
+      .form__item(:class="{ 'form__item--error': errors.title }")
+        label.form__label(for="title") Title
+        input(type="text", placeholder="Enter title...", v-model="title")
+        p.text-error(v-if="errors && errors.title")
+          | {{ errors.title[0] }}
+      .form__item
+        label.form__label(for="parent_id") Categories
+        select#parent_id(name="parent_id" v-model="parent_id")
+          option(value="0") No parent
+          option(v-for="item in categories" :key="item.id" :value="item.id") {{ item.title }}
+    button.btn(@click="onSubmit") Submit
 </template>
 <script>
 import AdminForm from "@/admin/form/Form";
+
 export default {
+  layout: "admin",
   data() {
     return {
-      name: "",
-      status: 1,
+      title: "",
+      parent_id: 0,
+      categories: [],
       errors: {},
     };
   },
   methods: {
     onSubmit(e) {
       e.preventDefault();
-      const data = { name: this.name, status: this.status };
+      const data = {title: this.title, status: this.status, parent_id: this.parent_id};
 
       this.$axios
-        .post("/category", data)
-        .then((res) => {
-          this.$router.push("/category");
-        })
-        .catch((err) => {
-          if (err.response.data && err.response.data.errors) {
-            this.errors = err.response.data.errors;
-          }
-        });
+          .post("/auth/category", data)
+          .then(() => {
+            this.$router.push("/admin/category");
+          })
+          .catch((err) => {
+            if (err.response.data && err.response.data.errors) {
+              this.errors = err.response.data.errors;
+            }
+          });
+    },
+    getCategories() {
+      this.$axios
+          .get("/auth/category")
+          .then((res) => {
+            const categories = res.data.data;
+            this.categories = categories.filter((item) => {
+              return item.parent_id === 0;
+            });
+            console.log(this.categories, "this.categories");
+          })
+          .catch((err) => {
+            if (err.response.data && err.response.data.errors) {
+              setErrors(err.response.data.errors);
+            }
+          });
     },
   },
   components: {
     AdminForm,
+  },
+  created() {
+    this.getCategories();
   },
 };
 </script>
