b912bb2d0acb286711616b09c847dcb7e301156f
serii <seriiburduja@gmail.com>
2022-09-09 09:50:15 +0300

    feat: product list and added edit

--- pages/admin/product/_id.vue
+++ pages/admin/product/_id.vue
@@ -1,44 +1,108 @@
-<template>
-  <AdminForm label="Create category">
-    <div class="form__flex">
-      <div class="form__item" :class="{ 'form__item--error': errors.name }">
-        <label class="form__label" htmlFor="name"> Name </label>
-        <input type="text" placeholder="Enter name..." v-model="name" />
-        <p v-if="errors && errors.name" class="text-error">
-          {{ errors.name[0] }}
-        </p>
-      </div>
-      <div class="form__item">
-        <label class="form__label" htmlFor="status"> Status </label>
-        <select name="status" id="status" v-model="status">
-          <option :value="1">Active</option>
-          <option :value="0">Inactive</option>
-        </select>
-      </div>
-    </div>
-    <button class="btn" @click="onSubmit">Submit</button>
-  </AdminForm>
+<template lang="pug">
+AdminForm(label="Edit category")
+  .form__flex
+    .form__item(:class="{ 'form__item--error': errors.title }")
+      label.form__label(for="title") Title
+      input(type="text", placeholder="Enter title...", v-model="title")
+      p.text-error(v-if="errors && errors.title")
+        | {{ errors.title[0] }}
+    .form__item
+      label.form__label(for="category_id") Category
+      select#category_id(name="category_id", v-model="category_id")
+        option(v-for="item in categories", :key="item.id", :value="item.id") {{ item.title }}
+      p.text-error(v-if="errors && errors.category_id")
+        | {{ errors.category_id[0] }}
+  .form__item(:class="{ 'form__item--error': errors.content }")
+    label.form__label(for="content") Content
+    mavon-editor(v-model="content", language="en")
+    p.text-error(v-if="errors && errors.content")
+      | {{ errors.content[0] }}
+  .form__flex
+    .form__item(:class="{ 'form__item--error': errors.description }")
+      label.form__label(for="description") Description
+      textarea(v-model="description")
+      p.text-error(v-if="errors && errors.description")
+        | {{ errors.description[0] }}
+    .form__item
+      label.form__label(htmlfor="status") Status
+      select#status(name="status", v-model="status")
+        option(:value="1") Active
+        option(:value="0") Inactive
+  .form__flex
+    .form__item(:class="{ 'form__item--error': errors.price }")
+      label.form__label(for="price") Price
+      input(type="text", v-model="price")
+      p.text-error(v-if="errors && errors.price")
+        | {{ errors.price[0] }}
+    .form__item(:class="{ 'form__item--error': errors.old_price }")
+      label.form__label(for="old_price") Old price
+      input(type="text", v-model="old_price")
+      p.text-error(v-if="errors && errors.old_price")
+        | {{ errors.old_price[0] }}
+  .form__flex
+    .form__item(:class="{ 'form__item--error': errors.hit }")
+      label.form__label(for="hit") 
+        input#hit(type="checkbox", v-model="hit", value="0") 
+        | Hit
+      p.text-error(v-if="errors && errors.hit")
+        | {{ errors.hit[0] }}
+    .form__item
+      label.form__label(htmlfor="img") Cover image
+      button.btn.btn--success(@click="coverImageHandler")
+        | Add image
+      p.form__url(v-if="img") {{ img }}
+      p.text-error(v-if="errors.img")
+        | {{ errors.img[0] }}
+  AdminMedia(
+    v-if="showMedia",
+    @on-close="closeMedia",
+    @on-images="setMediaImages"
+  )
+  button.btn(@click="onSubmit") Submit
 </template>
 <script>
+import AdminMedia from "@/admin/media/AdminMedia.vue";
 import AdminForm from "@/admin/form/Form";
 export default {
+  layout: "admin",
   data() {
     return {
-      id: "",
-      name: "",
+      title: "",
+      category_id: "",
+      brand_id: 1,
+      content: "",
+      price: 0,
+      old_price: 0,
       status: 1,
+      description: "",
+      img: "",
+      hit: "0",
       errors: {},
+      categories: [],
+      showMedia: false,
+      images: []
     };
   },
   methods: {
     onSubmit(e) {
       e.preventDefault();
-      const data = { name: this.name, status: this.status };
+      const data = {
+        title: this.title,
+        category_id: this.category_id,
+        brand_id: this.brand_id,
+        content: this.content,
+        price: this.price,
+        old_price: this.old_price,
+        status: this.status,
+        description: this.description,
+        img: this.img,
+        hit: this.hit,
+      };
 
       this.$axios
-        .put("/category/" + this.id, data)
-        .then(() => {
-          this.$router.push("/category");
+        .post("/auth/product", data)
+        .then((res) => {
+          /* this.$router.push("/category"); */
         })
         .catch((err) => {
           if (err.response.data && err.response.data.errors) {
@@ -46,27 +110,35 @@ export default {
           }
         });
     },
-    getData() {
-      this.$axios
-        .get("/category/" + this.id)
-        .then((res) => {
-          const { name, status } = res.data.data;
-          this.name = name;
-          this.status = status;
-        })
-        .catch((err) => {
-          if (err.response.data && err.response.data.errors) {
-            setErrors(err.response.data.errors);
-          }
+    getCategories() {
+      this.$axios.get("/auth/category").then((res) => {
+        this.categories = res.data.data;
+        this.categories = this.categories.filter((item) => {
+          return item.parent_id !== 0;
         });
+        console.log(this.categories, "this.categories");
+      });
+    },
+    closeMedia() {
+      document.body.style.overflow = "initial";
+      this.showMedia = false;
+    },
+    setMediaImages(images) {
+      this.media_images = images;
+      this.img = images[0];
+    },
+    coverImageHandler() {
+      document.body.style.overflow = "hidden";
+      this.showMedia = true;
     },
   },
   components: {
     AdminForm,
+    AdminMedia,
   },
   created() {
-    this.id = this.$route.params.id;
-    this.getData();
+    this.getCategories();
   },
 };
 </script>
+
--- pages/admin/product/index.vue
+++ pages/admin/product/index.vue
@@ -1,33 +1,36 @@
 <template lang="pug">
-  AdminForm(label='Products')
-    AdminTable
-      table
-        thead
-          tr
-            th #ID
-            th Title
-            th Category
-            th Sum
-            th Status
-            th Action
-        tbody
-          tr(v-for='item in data', :key='item.id')
-            td {{ item.id }}
-            td {{ item.title }}
-            td {{ item.category }}
-            td {{ item.price }}
-            td {{ item.status === '1' ? 'On' : 'Off' }}
-            td
-              nuxt-link.btn.btn--success(:to='`/admin/product/` + item.id') Edit
-              button.btn.btn--danger(@click='deleteItem(item.id)')
-                | Delete
+AdminForm(label="Products")
+  AdminTable
+    table
+      thead
+        tr
+          th #ID
+          th Img
+          th Title
+          th Category
+          th Sum
+          th Status
+          th Action
+      tbody
+        tr(v-for="item in data", :key="item.id")
+          td {{ item.id }}
+          td 
+            img(:src="`${server_url}/uploads/${item.img}`", alt="img", width="100")
+          td {{ item.title }}
+          td {{ item.category }}
+          td {{ item.price }}
+          td {{ item.status === '1' ? 'On' : 'Off' }}
+          td
+            nuxt-link.btn.btn--success(:to="`/admin/product/` + item.id") Edit
+            button.btn.btn--danger(@click="deleteItem(item.id)")
+              | Delete
 </template>
 <script>
 import AdminForm from "@/admin/form/Form.vue";
 import AdminTable from "@/admin/form/AdminTable.vue";
 
 export default {
-  layout: 'admin',
+  layout: "admin",
   data() {
     return {
       data: [],
@@ -40,23 +43,28 @@ export default {
   methods: {
     getData() {
       this.$axios
-          .get("/auth/product")
-          .then((res) => {
-            this.data = res.data.data.reverse();
-          })
-          .catch((err) => {
-            console.log(err.response, "err.response");
-          });
+        .get("/auth/product")
+        .then((res) => {
+          this.data = res.data.data.reverse();
+        })
+        .catch((err) => {
+          console.log(err.response, "err.response");
+        });
     },
     deleteItem(id) {
       this.$axios
-          .delete("/auth/product/" + id)
-          .then(() => {
-            this.getData();
-          })
-          .catch((err) => {
-            console.log(err.response.data.message, "err.response");
-          });
+        .delete("/auth/product/" + id)
+        .then(() => {
+          this.getData();
+        })
+        .catch((err) => {
+          console.log(err.response.data.message, "err.response");
+        });
+    },
+  },
+  computed: {
+    server_url() {
+      return this.$store.state.server_url;
     },
   },
   created() {
