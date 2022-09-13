f2a4504eb4a4b149510eb0eb0707dc06cfaa3d55
serii <seriiburduja@gmail.com>
2022-09-13 17:32:53 +0300

    feat: edit product attribute bug

--- components/admin/utils/AtributeComponent.vue
+++ components/admin/utils/AtributeComponent.vue
@@ -1,23 +1,30 @@
 <template lang="pug">
-  .attribute-component
-    header.attribute-component__tabs
-      .attribute-component__tab(
-        v-for="(item, index) in attributes",
-        :key="index",
-        :class="{ 'attribute-component__tab--active': activeTab === index }",
-        @click="activeTab = index"
-      ) {{ item.title }}
-    .attribute-component__body
-      .attribute-component__item(
-        v-for="(item, index) in attributes"
-        :key="index"
-        :class="{ 'attribute-component__item--active': activeTab === index }"
-      )
-        ul
-          li(@click="uncheck(item.title)") Uncheck
-          li(v-for="(value, index) in item.values", :key="index")
-            input(type="radio", :id="value.id", :name="value.title", :value="value.id", v-model="values[item.title]", @change="changeHandler({title: item.title, value: value.id})")
-            label(:for="value.id") {{ value.title }}
+.attribute-component
+  header.attribute-component__tabs
+    .attribute-component__tab(
+      v-for="(item, index) in attributes",
+      :key="index",
+      :class="{ 'attribute-component__tab--active': activeTab === index }",
+      @click="activeTab = index"
+    ) {{ item.title }}
+  .attribute-component__body
+    .attribute-component__item(
+      v-for="(item, index) in attributes",
+      :key="index",
+      :class="{ 'attribute-component__item--active': activeTab === index }"
+    )
+      ul
+        li(@click="uncheck(item.title)") Uncheck
+        li(v-for="(value, index) in item.values", :key="index")
+          input(
+            type="radio",
+            :id="value.id",
+            :name="value.title",
+            :value="value.id",
+            v-model="values[item.title]",
+            @change="changeHandler({ title: item.title, value: value.id })"
+          )
+          label(:for="value.id") {{ value.title }}
 </template>
 <script>
 export default {
@@ -26,24 +33,28 @@ export default {
       type: String,
       required: true,
     },
+    attrs: {
+      type: Array,
+      default: () => [],
+    },
   },
   data() {
     return {
       attributes: [],
       activeTab: 0,
       values: {},
-      result: {}
-    }
+      result: {},
+    };
   },
   methods: {
     uncheck(title) {
       this.values[title] = false;
       delete this.result[title];
-      console.log(this.result, 'this.result');
+      console.log(this.result, "this.result");
     },
     changeHandler(data) {
       this.result[data.title] = data.value;
-      this.$emit('on-change', this.result);
+      this.$emit("on-change", this.result);
     },
     getAttributes() {
       this.$axios.get(this.url).then((res) => {
@@ -52,9 +63,13 @@ export default {
       });
     },
   },
+  watch: {
+    attrs(value) {
+      this.attributes = value;
+    },
+  },
   created() {
     this.getAttributes();
-    console.log(this.values, 'this.values');
-  }
+  },
 };
 </script>
--- pages/admin/product/_id.vue
+++ pages/admin/product/_id.vue
@@ -1,104 +1,103 @@
 <template lang="pug">
-  AdminForm(label="Edit product")
-    .form__flex
-      .form__item(:class="{ 'form__item--error': errors.title }")
-        label.form__label(for="title") Title
-        input(type="text", placeholder="Enter title...", v-model="title")
-        p.text-error(v-if="errors && errors.title")
-          | {{ errors.title[0] }}
-      .form__item
-        label.form__label(for="category_id") Category
-        select#category_id(name="category_id", v-model="category_id")
-          option(v-for="item in categories", :key="item.id", :value="item.id") {{ item.title }}
-        p.text-error(v-if="errors && errors.category_id")
-          | {{ errors.category_id[0] }}
-
-    .form__item(:class="{ 'form__item--error': errors.content }")
-      label.form__label(for="content") Content
-      client-only
-        mavon-editor(v-model="content", language="en")
-      p.text-error(v-if="errors && errors.content")
-        | {{ errors.content[0] }}
-
-    .form__flex
-      .form__item(:class="{ 'form__item--error': errors.description }")
-        label.form__label(for="description") Description
-        textarea(v-model="description")
-        p.text-error(v-if="errors && errors.description")
-          | {{ errors.description[0] }}
-      .form__item
-        label.form__label(htmlfor="status") Status
-        select#status(name="status", v-model="status")
-          option(:value="1") Active
-          option(:value="0") Inactive
-
-    .form__flex
-      .form__item(:class="{ 'form__item--error': errors.price }")
-        label.form__label(for="price") Price
-        input(type="text", v-model="price")
-        p.text-error(v-if="errors && errors.price")
-          | {{ errors.price[0] }}
-      .form__item(:class="{ 'form__item--error': errors.old_price }")
-        label.form__label(for="old_price") Old price
-        input(type="text", v-model="old_price")
-        p.text-error(v-if="errors && errors.old_price")
-          | {{ errors.old_price[0] }}
-
-    .form__flex
-      .form__item(:class="{ 'form__item--error': errors.hit }")
-        label.form__label(for="hit")
-          input#hit(type="checkbox", v-model="hit", value="0")
-          | Hit
-        p.text-error(v-if="errors && errors.hit")
-          | {{ errors.hit[0] }}
-
+AdminForm(label="Edit product")
+  .form__flex
+    .form__item(:class="{ 'form__item--error': errors.title }")
+      label.form__label(for="title") Title
+      input(type="text", placeholder="Enter title...", v-model="title")
+      p.text-error(v-if="errors && errors.title")
+        | {{ errors.title[0] }}
     .form__item
-      label.form__label Related products
-      AutoComplete(
-        @on-select="searchHandler",
-        url="/auth/product-search?search="
-        :defaultValues="relations"
-      )
+      label.form__label(for="category_id") Category
+      select#category_id(name="category_id", v-model="category_id")
+        option(v-for="item in categories", :key="item.id", :value="item.id") {{ item.title }}
+      p.text-error(v-if="errors && errors.category_id")
+        | {{ errors.category_id[0] }}
 
+  .form__item(:class="{ 'form__item--error': errors.content }")
+    label.form__label(for="content") Content
+    client-only
+      mavon-editor(v-model="content", language="en")
+    p.text-error(v-if="errors && errors.content")
+      | {{ errors.content[0] }}
+
+  .form__flex
+    .form__item(:class="{ 'form__item--error': errors.description }")
+      label.form__label(for="description") Description
+      textarea(v-model="description")
+      p.text-error(v-if="errors && errors.description")
+        | {{ errors.description[0] }}
     .form__item
-      label.form__label Filters(Attributes)
-      AttributeComponent(url="/auth/attribute", @on-change="attributeHandler")
+      label.form__label(htmlfor="status") Status
+      select#status(name="status", v-model="status")
+        option(:value="1") Active
+        option(:value="0") Inactive
 
-    .form__flex
-      .form__item
-        label.form__label(for="img") Cover image
-        button.btn.btn--success(@click="coverImageHandler")
-          | Add image
-        br
-        br
-        div
-          img(v-if="img" :src="`${server_url}${img}`" :width="100" alt="item")
-        p.text-error(v-if="errors.img")
-          | {{ errors.img[0] }}
+  .form__flex
+    .form__item(:class="{ 'form__item--error': errors.price }")
+      label.form__label(for="price") Price
+      input(type="text", v-model="price")
+      p.text-error(v-if="errors && errors.price")
+        | {{ errors.price[0] }}
+    .form__item(:class="{ 'form__item--error': errors.old_price }")
+      label.form__label(for="old_price") Old price
+      input(type="text", v-model="old_price")
+      p.text-error(v-if="errors && errors.old_price")
+        | {{ errors.old_price[0] }}
 
-      .form__item
-        label.form__label Gallery
-        button.btn.btn--success(@click="galleryHandler")
-          | Add image
-        ul.form__gallery(v-if="gallery && gallery.length")
-          li(v-for="item in gallery" :key="item")
-            img(:src="`${server_url}${item}`" :width="100" alt="item")
+  .form__flex
+    .form__item(:class="{ 'form__item--error': errors.hit }")
+      label.form__label(for="hit")
+        input#hit(type="checkbox", v-model="hit", value="0")
+        | Hit
+      p.text-error(v-if="errors && errors.hit")
+        | {{ errors.hit[0] }}
 
-    AdminMedia(
-      v-if="showMedia",
-      @on-close="closeMedia",
-      @on-images="setMediaImages"
+  .form__item
+    label.form__label Related products
+    AutoComplete(
+      @on-select="searchHandler",
+      url="/auth/product-search?search=",
+      :defaultValues="relations"
     )
 
-    AdminMedia(
-      v-if="showMediaGallery",
-      @on-close="closeMedia",
-      @on-gallery="setMediaGallery"
-      :is_gallery="true"
-    )
+  .form__item
+    label.form__label Filters(Attributes)
+    AttributeComponent(url="/auth/attribute", @on-change="attributeHandler", :attrs="attributes")
 
-    button.btn.form__submit(@click="onSubmit") Submit
+  .form__flex
+    .form__item
+      label.form__label(for="img") Cover image
+      button.btn.btn--success(@click="coverImageHandler")
+        | Add image
+      br
+      br
+      div
+        img(v-if="img", :src="`${server_url}${img}`", :width="100", alt="item")
+      p.text-error(v-if="errors.img")
+        | {{ errors.img[0] }}
 
+    .form__item
+      label.form__label Gallery
+      button.btn.btn--success(@click="galleryHandler")
+        | Add image
+      ul.form__gallery(v-if="gallery && gallery.length")
+        li(v-for="item in gallery", :key="item")
+          img(:src="`${server_url}${item}`", :width="100", alt="item")
+
+  AdminMedia(
+    v-if="showMedia",
+    @on-close="closeMedia",
+    @on-images="setMediaImages"
+  )
+
+  AdminMedia(
+    v-if="showMediaGallery",
+    @on-close="closeMedia",
+    @on-gallery="setMediaGallery",
+    :is_gallery="true"
+  )
+
+  button.btn.form__submit(@click="onSubmit") Submit
 </template>
 <script>
 import AdminMedia from "@/admin/media/AdminMedia.vue";
@@ -188,6 +187,7 @@ export default {
           img,
           hit,
           related,
+          attributes,
         } = res.data.data;
         this.title = title;
         this.category_id = category_id;
@@ -201,7 +201,8 @@ export default {
         this.hit = hit;
         this.searchHandler(related);
         this.relations = JSON.parse(related);
-        console.log(this.relations, 'this.relations');
+        this.attributes = attributes;
+        console.log(this.attributes, "this.attributes");
       });
     },
     closeMedia() {
